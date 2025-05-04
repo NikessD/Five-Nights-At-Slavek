cca89144b47f1c45bf6e3e37f752513b8a2f7678
NikessD <k.mauer@seznam.cz>
2025-05-04 22:39:09 +0200

    Minor ui changes

--- scripts/win_screen.gd
+++ scripts/win_screen.gd
@@ -5,8 +5,8 @@ func _ready() -> void:
 	GlobalVars.config = ConfigFile.new()
 	GlobalVars.config.save("res://save.cfg")
 	$"Deep-strange-whoosh-183845".play()
-	$Container/Night.text = "NIGHT " + str(GlobalVars.night)  
-	
+	$CenterContainer/Night.text = "NIGHT " + str(GlobalVars.night)  
+
 	
 	
 func _process(delta: float) -> void:
@@ -20,6 +20,6 @@ func _on_timer_timeout() -> void:
 
 func _on_timer_night_timeout() -> void:
 	GlobalVars.night += 1
-	$Container/Night.text = "NIGHT " + str(GlobalVars.night) 
+	$CenterContainer/Night.text = "NIGHT " + str(GlobalVars.night) 
 	GlobalVars.config.set_value("night number", "night", GlobalVars.night)
 	GlobalVars.config.save("res://save.cfg")
